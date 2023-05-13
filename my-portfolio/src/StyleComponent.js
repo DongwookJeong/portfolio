054804f8210013d5449d9518cc4f3fa4d5b051c1
DongwookJeong <nolhoon211@gmail.com>
2023-05-13 18:04:31 +0900

    웹 홈페이지 마크업 작업중

---
+++ my-portfolio/src/StyleComponent.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+const Whc = styled.div`
+  display: "flex";
+`;
+export default Whc;
--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -2,26 +2,34 @@ import { Link } from "react-router-dom";
 import { me } from "./About";
 import React from "react";
 import Mountain from "/Users/jactor/portfolio/portfolio/my-portfolio/src/img/mountain_apple.png";
-
+import "/Users/jactor/portfolio/portfolio/my-portfolio/src/pages/Home.scss";
 const Home = () => {
   return (
     <div>
-      <h1>home</h1>
-      <div>
-        <img alt="mountain_apple.png" src={Mountain} />
-        <ul>
-          <li>
-            <Link to="/abouts/dongwook">about</Link>
-            <p>{me.dongwook.description}</p>
+      <h1 className="Home">home</h1>
+      <di className="Whm">
+        <img className="WhmPfc" alt="mountain_apple.png" src={Mountain} />
+        <ul className="WhmUl">
+          <li className="WhmList">
+            <Link className="WhmLink" to="/abouts/dongwook">
+              about
+            </Link>
+            <p className="WhmListP">{me.dongwook.description}</p>
           </li>
-          <li>
-            <Link to="/Contect">contect</Link>
+          <li className="WhmList">
+            <Link className="WhmLink" to="/Contect">
+              contect
+            </Link>
+            <p className="WhmListP">깃허브 이미지?</p>
           </li>
-          <li>
-            <Link to="/Projects">projects</Link>
+          <li className="WhmList">
+            <Link className="WhmLink" to="/Projects">
+              projects
+            </Link>
+            <p className="WhmListP">프로젝트이미지</p>
           </li>
         </ul>
-      </div>
+      </di>
     </div>
   );
 };
