de069dfe4a18ab4684dc4040f8fbcb8a979718b6
DongwookJeong <nolhoon211@gmail.com>
2023-05-21 15:59:11 +0900

    고정 레이아웃 스타일링 작성 변경 및 다른 페이지로 이동 가능하게끔 페이지 버튼 추가

--- my-portfolio/src/HeadLayout.js
+++ my-portfolio/src/HeadLayout.js
@@ -1,4 +1,6 @@
 import { Outlet, useNavigate } from "react-router-dom";
+import styled from "styled-components";
+
 const HeadLayout = () => {
   const navigate = useNavigate();
 
@@ -6,64 +8,77 @@ const HeadLayout = () => {
     navigate("/");
   };
 
-  const sameStyle = {
-    width: "10px",
-    height: "10px",
-    borderRadius: "5px",
+  const about = () => {
+    navigate("/about");
   };
+
+  const contect = () => {
+    navigate("/contect");
+  };
+
+  const projects = () => {
+    navigate("/projects");
+  };
+  const Header = styled.header`
+    background: none;
+    display: flex;
+  `;
+
+  const HomeButton = styled.button`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    background: none;
+    margin: 1rem;
+    border: hidden;
+  `;
+
+  const Circle = styled.div`
+    width: 10px;
+    height: 10px;
+    border-radius: 5px;
+  `;
+
+  const OtherContent = styled.button`
+    font-size: 1rem;
+  `;
 
   return (
     <div>
-      <header
-        style={{
-          background: "none",
-          display: "flex",
-        }}
-      >
-        <button
-          onClick={comeBackhome}
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            flexDirection: "column",
-            alignItems: "center",
-            background: "none",
-            border: "hidden",
-            margin: "1rem",
-          }}
-        >
-          <div
+      <Header>
+        <HomeButton onClick={comeBackhome}>
+          <Circle
             style={{
               background: "#fcd201",
-              ...sameStyle,
               marginBottom: "0.6rem",
             }}
-          ></div>
+          ></Circle>
           <div style={{ display: "flex", justifyContent: "center" }}>
-            <div
+            <Circle
               style={{
                 background: "#8795A8",
-                ...sameStyle,
                 marginRight: "1rem",
               }}
-            ></div>
-            <div
+            ></Circle>
+            <Circle
               style={{
                 background: "#236144",
-                ...sameStyle,
                 marginLeft: "1rem",
               }}
-            ></div>
+            ></Circle>
           </div>
-          <div
+          <Circle
             style={{
               background: "#3359AD",
-              ...sameStyle,
               marginTop: "0.6rem",
             }}
-          ></div>
-        </button>
-      </header>
+          ></Circle>
+        </HomeButton>
+        <OtherContent onClick={about}>about</OtherContent>
+        <OtherContent onClick={contect}>contect</OtherContent>
+        <OtherContent onClick={projects}>projects</OtherContent>
+      </Header>
       <main style={{ height: "90vh" }}>
         <Outlet />
       </main>
