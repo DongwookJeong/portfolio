2b930d6fa49a367fada8e28b5fddc40ebef5a992
DongwookJeong <nolhoon211@gmail.com>
2023-05-28 19:21:05 +0900

    projectdata repository 주소 추가

--- my-portfolio/src/localdb/ProjectData.js
+++ my-portfolio/src/localdb/ProjectData.js
@@ -2,12 +2,12 @@ const ProjectData = {
   project1: {
     name: "꽃가루",
     description: "공공데이터 Api를 활요한 웹사이트",
-    github: "https://github.com/DongwookJeong",
+    github: "https://github.com/DongwookJeong/flower_portfolio",
   },
   project2: {
     name: "시지프스 형벌",
     description: "브라우저창 좌표를 활용한 간단한 게임",
-    github: "https://github.com/DongwookJeong",
+    github: "https://github.com/DongwookJeong/sisyphus_portfolio",
   },
 };
 
